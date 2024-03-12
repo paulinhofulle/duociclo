1f9244a346517b893f3a6af4fcfc0db07fa68f58
paulinhofulle <paulohfulle@gmail.com>
2024-03-12 20:03:35 -0300

    abc

--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -1,5 +1,6 @@
 @extends('site/layoutLogin')
-@section('title', 'Duociclo')
+{{-- @section('title', 'Duociclo') --}}
+@section('title', 'Fulle Sistemas')
 
 @section('form')
 <img src="/img/aralho.png" style="height: 30rem">
